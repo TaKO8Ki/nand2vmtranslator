efd0df43dc64bc23a896c7ace22642939021d402
TaKO8Ki <takoyaki0316@gmail.com>
2020-05-05 23:30:30 +0900

    add .gitginore

--- target/debug/deps/nand2vmtranslator-80ca719e4ab74d7e.d
+++
@@ -1,5 +0,0 @@
-/Users/tako8ki/GitHub/nand2vmtranslator/target/debug/deps/nand2vmtranslator-80ca719e4ab74d7e: src/main.rs
-
-/Users/tako8ki/GitHub/nand2vmtranslator/target/debug/deps/nand2vmtranslator-80ca719e4ab74d7e.d: src/main.rs
-
-src/main.rs:
--- target/debug/nand2vmtranslator-80ca719e4ab74d7e.d
+++
@@ -1,1 +0,0 @@
-/Users/tako8ki/GitHub/nand2vmtranslator/target/debug/nand2vmtranslator-80ca719e4ab74d7e: /Users/tako8ki/GitHub/nand2vmtranslator/src/main.rs
