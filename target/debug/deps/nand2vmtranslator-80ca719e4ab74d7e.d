526be278b06d9ddb1bb2ae1ba6f1cb0c8540bbd7
TaKO8Ki <takoyaki0316@gmail.com>
2020-05-05 23:26:30 +0900

    first commit

---
+++ target/debug/deps/nand2vmtranslator-80ca719e4ab74d7e.d
@@ -0,0 +1,5 @@
+/Users/tako8ki/GitHub/nand2vmtranslator/target/debug/deps/nand2vmtranslator-80ca719e4ab74d7e: src/main.rs
+
+/Users/tako8ki/GitHub/nand2vmtranslator/target/debug/deps/nand2vmtranslator-80ca719e4ab74d7e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/nand2vmtranslator-80ca719e4ab74d7e.d
@@ -0,0 +1,1 @@
+/Users/tako8ki/GitHub/nand2vmtranslator/target/debug/nand2vmtranslator-80ca719e4ab74d7e: /Users/tako8ki/GitHub/nand2vmtranslator/src/main.rs
